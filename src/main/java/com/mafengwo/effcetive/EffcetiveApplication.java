9fc69615b9ee3d6931fe8a32cef47a142200a245
maxraycc <maxraycc@163.com>
2020-05-15 10:41:31 +0800

    Initial commit

---
+++ src/main/java/com/mafengwo/effcetive/EffcetiveApplication.java
@@ -0,0 +1,13 @@
+package com.mafengwo.effcetive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffcetiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffcetiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mafengwo/effcetive/EffcetiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafengwo.effcetive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffcetiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
